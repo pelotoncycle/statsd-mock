c38c2f5cf5c9b20d694c45da8c93dcdf23e1ce52
Salton Arthur Massally <salton.massally@gmail.com>
2016-01-04 14:18:25 +0000

    update path to license file

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mock-statsd',
+    name='statsdmock',
     version='0.0.1',
     description='statsd mock server based on gevent',
     author='Salton Massally',
     author_email='salton.massally@gmail.com',
     url='http://github.com/tarzan0820/mock-statsd',
     packages=find_packages(),
-    license=open('LICENSE').read(),
+    license=open('LICENSE.txt').read(),
     include_package_data=True,
     install_requires=['gevent', 'statsd'],
     tests_require=['nose'],
-    test_suite='nose.collector'
+    test_suite='nose.collector',
+    keywords = ['testing', 'logging'],
+    classifiers = [],
 )
